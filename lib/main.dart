c308ec20185e9cda92cb9c975eb45d16f2c6ebe9
koushik2002vas5 <koushiksai.vas5@gmail.com>
2025-04-07 20:35:29 -0400

    Finally made some bug fixes

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Firebase Auth App',
+      title: 'Firebase Authentication App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
